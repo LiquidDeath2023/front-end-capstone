0c46ab30c06905f51e7a7029643a7b1e30044c66
benjamin <bnt922@gmail.com>
2023-08-10 17:07:04 -0400

    B Added Star Symbol in StarsAndReview

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,7 +15,6 @@ import Overview from './components/Overview';
 import ReviewsRatings from './components/Reviews-Ratings/ReviewsRatings';
 >>>>>>> 4c6f0c2 (Structure overview file system and add placeholder data for Product Info)
 
-// nothing special here
 function App() {
   return (
     <div>
