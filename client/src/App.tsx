c3725430768e013302f6e0215099a79b639955ac
benjamin <bnt922@gmail.com>
2023-08-10 17:07:04 -0400

    B Added Star Symbol in StarsAndReview

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,6 @@ import './app.css';
 import QuestionsAndAnswers from './components/QuestionsAndAnswers';
 import Overview from './components/Overview';
 import ReviewsRatings from './components/Reviews-Ratings/ReviewsRatings';
-import { List } from './components/Related/Helpers/List';
-import { Card } from './components/Related/Card';
 function App() {
   return (
     <div>
--- client/src/components/Overview/Features/ProductInfo/Subcomponents/StarsAndReviews.tsx
+++ client/src/components/Overview/Features/ProductInfo/Subcomponents/StarsAndReviews.tsx
@@ -1,37 +1,23 @@
 import React from "react";
 
 function StarsAndReviews({ reviews }) {
-  // finds average of reviews
   function reviewAvg() {
     let total = 0;
     reviews.results.forEach((review) => {
       total += review.rating;
     });
     const average = total / reviews.results.length;
-    console.log(average);
     return average;
   }
-  // creates stars
-  const reviewStars = (num) => {
-    const stars = [];
-    for (let i = 0; i < 5; i++) {
-      if (i < Math.floor(num)) {
-        stars.push(<i className="star fa-regular fa-star" />);
-      } else if (i - Math.floor(num) < 1) {
-        const percent = (num - Math.floor(num)) * 100;
-        stars.push(
-          <i className="star fa-regular fa-star" style={{ width: percent }} />
-        );
-      } else {
-        stars.push(<i className="empty-star fa-regular fa-star" />);
-      }
-    }
-    return stars;
+  // if u get 3.8, should be 3 stars and 80% of last star
+  const stars = () => {
+    const string = "hello";
+    return string;
   };
   return (
     <div>
-      <div className="star-container">{reviewStars(reviewAvg())}</div>
-      <h4>Read all reviews</h4>
+      <i className="star fa-regular fa-star" />
+      <h4>1 1 1 0.75 0 Read all reviews</h4>
     </div>
   );
 }
