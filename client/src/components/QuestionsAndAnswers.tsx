68ef4fdd5eed1eceff23d825bd2c1b0580d46e7e
Khurram Ali <khurramcali@gmail.com>
2023-08-11 00:34:20 -0400

    serarch filter v1 setup

--- client/src/components/QuestionsAndAnswers.tsx
+++ client/src/components/QuestionsAndAnswers.tsx
@@ -22,7 +22,7 @@ import React, { useState } from 'react';
 import React, { useState, useEffect } from 'react';
 >>>>>>> a4ddec7 (completed clickables)
 import './QuestionsAndAnswers.css';
-// import Search from './Questions-Answers/Search';
+import Search from './Questions-Answers/Search';
 import QandA from './Questions-Answers/QandA';
 import ExpandAndAdd from './Questions-Answers/ExpandAndAdd';
 import myData from './Questions-Answers/Helpers/fakeData';
@@ -39,7 +39,7 @@ function QuestionsAndAnswers() {
   useEffect(() => {
     setFakeData(listOfQuestions.slice(0, numOfQuestions));
   }, [numOfQuestions]);
-  console.log(listOfQuestions);
+  // console.log(listOfQuestions);
   const add2 = () => {
     setNumOfQuestions(numOfQuestions + 2);
     if (numOfQuestions + 2 >= A) {
@@ -52,11 +52,23 @@ function QuestionsAndAnswers() {
     setLoadQuestions(true);
   };
 
+  const filterQs = (query) => {
+    // only checks question edit for answers later
+    // try to reset search to 4 at a time;
+    const filtered = listOfQuestions.filter(question => question.question_body.toLowerCase().includes(query.toLowerCase()));
+
+    if (query.length >= 3) {
+      setFakeData(filtered);
+    } else {
+      setFakeData(listOfQuestions);
+    }
+  };
+
   return (
     <div className="qa-main-container">
       <div>QUESTIONS & ANSWERS</div>
-      <div>SEARCH COMPONENT HIDDEN</div>
-      {/* <Search /> */}
+      {/* <div>SEARCH COMPONENT HIDDEN</div> */}
+      <Search filterQs={filterQs} />
       <QandA fakeData={fakeData} />
       <ExpandAndAdd add2={add2} collapse={collapse} loadQuestions={loadQuestions} />
       {/* <div>EXPAND AND ADD COMPONENT HIDDEN</div> */}
